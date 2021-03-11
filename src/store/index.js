dcc2451edb98894bc33bc5dc5b35405e4eff2be9
Essentiel16 <folarinabigail2020@gmail.com>
2021-03-11 13:45:20 +0100

    covid-19 project

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,71 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import VuexPersistence from 'vuex-persist'
+
+const vuexLocal = new VuexPersistence({
+  storage: window.localStorage,
+  key: 'covidwebpage'
+})
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    allPost: {},
+    europeRegion: [],
+    allNation: {},
+    loading: true
   },
   mutations: {
+    setAllPost: (state, payload) => {
+      state.allPost = payload
+    },
+    setAllNation: (state, payload) => {
+      state.allNation = payload
+    },
+    setEuropeRegion: (state, payload) => {
+      state.europeRegion = payload
+    }
   },
   actions: {
+    getAllPost (state) {
+      const baseURL = 'https://covid-api.mmediagroup.fr/v1/cases'
+      fetch(baseURL)
+        .then(response => response.json())
+        .then(data => {
+          console.log(data.Global)
+          console.log(Object.entries(data))
+          state.commit('setAllPost', data)
+        // .catch(error => console.log(error))
+        })
+    },
+    getEuropeRegions (state) {
+      const continentData = ['Europe', 'Americas', 'Asia', 'Africa', 'Oceania'].map(async (name) => {
+        const data = await fetch(`https://covid-api.mmediagroup.fr/v1/cases?continent=${name}`)
+        const response = await data.json()
+        console.log(response)
+        return response
+      })
+      Promise.all(continentData)
+      continentData.forEach((e) => {
+        getEuropeRegion.push(Object.entries(e))
+        console.log(e)
+      })
+    }
+  },
+  getters: {
+    getGlobalData: state => {
+      return state.allPost.Global.All
+    },
+    getCountries: state => {
+      const countries = Object.entries(state.allPost)
+      return countries.filter((country) => country[0] !== 'Global')
+    },
+    getEuropeRegion: state => {
+      return state.europeRegion
+    }
   },
   modules: {
-  }
+  },
+  plugins: [vuexLocal.plugin]
 })
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,105 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+      <h1 class="welcome">{{msg}}</h1>
+    <div class="data-info">
+      <h1 v-if="loading">Loading...</h1>
+      <div class="covid-data" >
+        <h3>Total Confirmed: {{globalData.confirmed}}</h3>
+        <p>Total Recovered: {{globalData.recovered}}</p>
+        <p>Total Deaths: {{globalData.deaths}}</p>
+      </div>
+      <br>
+      <div>
+        <h3>Total Cases in Europe: {{europeRegion.confirmed}}</h3>
+        <h3>Total Cases in America: {{europeRegion}}</h3>
+        <h3>Total Cases in Asia: {{europeRegion}}</h3>
+        <h3>Total Cases in Africa: {{europeregion}}</h3>
+        <h3>Total Cases in Oceania: {{europeregion}}</h3>
+      </div>
+      <div>
+        <table>
+          <thead>
+            <tr>
+              <td>Country</td>
+              <td>Confirmed Cases</td>
+              <td>Recoveries</td>
+              <td>Deaths</td>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="(country, index) in countries" :key="index">
+              <td>{{country[0]}}</td>
+              <td>{{country[1].All.confirmed}}</td>
+              <td>{{country[1].All.recovered}}</td>
+              <td>{{country[1].All.deaths}}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import { mapActions, mapGetters } from 'vuex'
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      msg: 'Welcome to the HomePage',
+      loading: true,
+      allPost: {}
+    }
+  },
+  mounted () {
+    this.getAllPost()
+    this.getEuropeRegions()
+  },
+  methods: {
+    ...mapActions({
+      getAllPost: 'getAllPost',
+      getEuropeRegions: 'getEuropeRegions'
+    })
+  },
+  computed: {
+    ...mapGetters({
+      countries: 'getCountries',
+      globalData: 'getGlobalData',
+      europeRegion: 'getEuropeRegion'
+    })
   }
 }
 </script>
+<style scoped>
+.data-info{
+  display: grid;
+  justify-content: center;
+  align-content: center;
+  margin-top: 25px;
+}
+.covid-data{
+border: 1px solid #333333;
+border-radius: 6px;
+padding:16px;
+}
+.welcome{
+  font-size: 48px;
+  font-weight: 700;
+  font-style: italic;
+  text-align: center;
+  margin-top: 10px;
+  margin-bottom: 10px;
+  text-shadow: 3px 6px rgba(255, 255, 255, 0.25);
+}
+.total-countries{
+  margin: auto;
+}
+.total-countries tr{
+  display: flex;
+  gap: 30px;
+  justify-content: center;
+  align-content: center;
+  padding: 10px;
+}
+</style>
